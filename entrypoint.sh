e6fa97612b8d20c30cac3ac673b7cc863e1c34f0
tooliload <tinolwl2@aol.com>
2023-07-24 21:01:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 # Entrypoint for idrive
 echo "iDrive start.."
 
+echo "Update iDrive Deps"
+/work/IDriveForLinux/scripts/account_setting.pl true
+
 echo "Checking for updates to iDrive perl scripts"
 /work/IDriveForLinux/scripts/check_for_update.pl silent
+
+
 
 # Docker Logs
 tail -f /var/log/idrive.log
